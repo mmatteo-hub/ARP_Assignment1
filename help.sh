4b31169b46e2beaa2bb68d5b2a3b9da1a1a2a727
teo_ubu <mattemara99@gmail.com>
2021-12-01 20:11:59 +0100

    modified help.sh

--- help.sh
+++ help.sh
@@ -1,7 +1,4 @@
 more README.txt
-
-cd -- "$(find . -iname src -type d)"
-
 more src/commandconsole/commandconsole.txt
 more src/inspectionconsole/inspectionconsole.txt
 more src/executor/executor.txt
