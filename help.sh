a6529aee2b92ac0b7fd15c5edf7a4244bfcfcfd4
teo_ubu <mattemara99@gmail.com>
2021-12-01 20:07:53 +0100

    modified help.sh

--- help.sh
+++ help.sh
@@ -1,1 +1,8 @@
+cd -- "$(find . -iname src -type d)"
 
+more src/commandconsole/commandconsole.txt
+more src/inspectionconsole/inspectionconsole.txt
+more src/executor/executor.txt
+more src/motorX/motorX.txt
+more src/motorZ/motorZ.txt
+more src/watchdog/watchdog.txt
