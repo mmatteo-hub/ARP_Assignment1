0d58916c8898ad310922f96befa92372af1d2940
teo_ubu <mattemara99@gmail.com>
2021-12-02 12:40:00 +0100

    added comments to the code

--- help.sh
+++ help.sh
@@ -1,3 +1,5 @@
+# adding path and file .txt to the help.sh file to show in the shell
+
 more src/commandconsole/commandconsole.txt
 more src/inspectionconsole/inspectionconsole.txt
 more src/executor/executor.txt
